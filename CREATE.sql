a857e09c001bcb2e3d68a6ccc9d21c8ff424c2cc
X. Y. Kevin Liu <x.y.kevin.liu@gmail.com>
2013-07-28 12:26:25 -0700

    added "not null"

--- CREATE.sql
+++ CREATE.sql
@@ -1,21 +1,19 @@
 CREATE TABLE Item
 (
-	upc 	CHAR(4) PRIMARY KEY,
-	title	VARCHAR(50),
-	type	ENUM('CD', 'DVD'),
+	upc 	CHAR(4) not null PRIMARY KEY,
+	title	VARCHAR(50) not null,
+	type	ENUM('CD', 'DVD') not null,
 	category ENUM('rock', 'pop', 'rap', 'country', 'classical', 'new age', 'instrumental') not null,
-	company	VARCHAR(50),
-	year	DATE,
-	price	DOUBLE,
-	stock	SMALLINT,
-
---	PRIMARY KEY (upc)
+	company	VARCHAR(50) not null,
+	year	DATE not null,
+	price	DOUBLE not null,
+	stock	SMALLINT not null,
 );
 
 CREATE TABLE LeadSinger
 (
-	upc	CHAR(4),
-	name	VARCHAR(50),
+	upc	CHAR(4) not null,
+	name	VARCHAR(50) not null,
 
 	PRIMARY KEY(upc, name),
 	FOREIGN KEY (upc) REFERENCES Item
@@ -24,7 +22,7 @@ CREATE TABLE LeadSinger
 CREATE TABLE HasSong
 (
 	upc	CHAR(4) not null,
-	title	VARCHAR(30),
+	title	VARCHAR(30) not null,
 	
 	PRIMARY KEY (upc, title),
 	FOREIGN KEY (upc) REFERENCES Item
@@ -32,9 +30,9 @@ CREATE TABLE HasSong
 
 CREATE TABLE Purchase
 (
-	receiptId	CHAR(30) PRIMARY KEY,-- TODO
-	pDate		DATE,
-	cid		CHAR(16),-- TODO
+	receiptId	CHAR(30) not null PRIMARY KEY,
+	pDate		DATE not null,
+	cid		CHAR(16),
 	cardNum		CHAR(16),
 	expiryDate	DATE,
 	expectedDate	DATE,
@@ -45,9 +43,9 @@ CREATE TABLE Purchase
 
 CREATE TABLE PurchaseItem
 (
-	receiptId	CHAR(30),
-	upc		CHAR(4),
-	quantity	SMALLINT,
+	receiptId	CHAR(30) not null,
+	upc		CHAR(4) not null,
+	quantity	SMALLINT not null,
 
 	PRIMARY KEY (receiptId, upc),
 	FOREIGN KEY (receiptId) REFERENCES Purchase,
@@ -56,27 +54,27 @@ CREATE TABLE PurchaseItem
 
 CREATE TABLE Customer
 (
-	cid		CHAR(16) PRIMARY KEY,
-	password	VARCHAR(25) NOT NULL,
-	name		VARCHAR(50),
-	address		VARCHAR(50),
-	phone		CHAR(10),
+	cid		CHAR(16) not null PRIMARY KEY,
+	pswd		VARCHAR(25) not null,
+	name		VARCHAR(50) not null,
+	address		VARCHAR(50) not null,
+	phone		CHAR(10) not null,
 );
 
 CREATE TABLE Return
 (
-	retid		CHAR(30) PRIMARY KEY,
-	rDate		DATE,
-	receiptId	CHAR(30),
+	retid		CHAR(30) not null PRIMARY KEY,
+	rDate		DATE not null,
+	receiptId	CHAR(30) not null,
 
 	FOREIGN KEY (receiptId) REFERENCES Purchase
 );
 
 CREATE TABLE ReturnItem
 (
-	retid		CHAR(30),
-	upc		CHAR(4),
-	quantity	SMALLINT,
+	retid		CHAR(30) not null,
+	upc		CHAR(4) not null,
+	quantity	SMALLINT not null,
 
 	PRIMARY KEY (retid, upc),
 	FOREIGN KEY (retid) REFERENCES Return,
