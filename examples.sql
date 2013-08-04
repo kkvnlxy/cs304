4bae7e1b4ed6704e7ce8489ca6086c70755ad83b
angewei25 <angelawei25@gmail.com>
2013-08-04 12:39:27 -0700

    items and customer examples

---
+++ examples.sql
@@ -0,0 +1,17 @@
+---------------------------------------------------------------
+-- Item:
+---------------------------------------------------------------
+insert into item values ('0001', 'We Can"t Stop', 'CD', 'POP', 'RCA Records', '2013', '1.29', '2');
+insert into item values ('0002', 'Believe', 'CD', 'POP', 'The Item Def Jam Music Group', '2012', '13.99', '6');
+insert into item values ('0003', 'Beethoven: The Piano Concertos', 'DVD', 'CLASSICAL', 'Deutsche Grammophon Studio', '2007', '34.99', '3');
+insert into item values ('0004', 'Grand Ole Opry at Carnegie Hall' 
+
+
+
+---------------------------------------------------------------
+-- Customer:
+---------------------------------------------------------------
+INSERT INTO Customer VALUES('kevin', '1234', 'Kevin Liu', 'Somewhere in Vancovuer', '778-123-4567');
+INSERT INTO Customer VALUES('kkvnlxy', '1234', 'Kevin Liu', 'Somewhere in Vancovuer', '778-123-4567');
+INSERT INTO Customer VALUES('ang', '2234', 'Angela Wei', 'Richmond', '778-213-1245');
+
