3b8c8203d01fb9dba7582dcb87a34ace9c4e1a4b
X. Y. Kevin Liu <x.y.kevin.liu@gmail.com>
2013-08-05 16:42:28 -0700

    auto-gen ID now has prefix

--- CREATE.sql
+++ CREATE.sql
@@ -70,22 +70,22 @@ CREATE TABLE RefundItem(
 -------------------------------------------------
 CREATE SEQUENCE ReceiptID_SEQ
 start with 1
-MAXVALUE 9999999999;
+MAXVALUE 999999999;
 CREATE or REPLACE TRIGGER ReceiptID_TG
 before insert on Purchase
 for each row
 begin
-	select to_char(ReceiptId_SEQ.nextval) into :new.receiptID from dual;
+	select concat('P', to_char(ReceiptId_SEQ.nextval)) into :new.receiptID from dual;
 end;
 /
 
 CREATE SEQUENCE RetID_SEQ
 start with 1
-MAXVALUE 9999999999;
+MAXVALUE 999999999;
 CREATE OR REPLACE TRIGGER RetID_TG
 before insert on Refund
 for each row
 begin
-	select to_char(RetID_SEQ.nextval) into :new.retID from dual;
+	select concat('R', to_char(RetID_SEQ.nextval)) into :new.retID from dual;
 end;
 /
